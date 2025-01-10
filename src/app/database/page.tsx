d868b394823b8ed36da97e94eea605310be4403a
Fannar <fannarjack@gmail.com>
2025-01-10 16:05:48 +0000

    as

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+  export const dynamic = "force-dynamics"
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
